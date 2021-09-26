d3654858b91c7ebf9a838a855074d1acb3351e51
oreoluwaolatunji23@gmail.com <oreoluwaolatunji23@gmail.com>
2021-09-26 15:27:24 +0100

    SCA-2 Swagger Configuration

--- src/main/java/com/example/safariwebstore008/configurations/SwaggerConfig.java
+++ src/main/java/com/example/safariwebstore008/configurations/SwaggerConfig.java
@@ -1,4 +1,22 @@
 package com.example.safariwebstore008.configurations;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.safariwebstore008"))
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
