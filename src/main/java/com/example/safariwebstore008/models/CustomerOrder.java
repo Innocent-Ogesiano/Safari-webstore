da0e23f19ec12295b90418427c070c7d0776364f
iyage <iyaghe.ofunrein@decagon.dev>
2021-09-28 14:18:16 +0100

    This make all the class extends the base class

---
+++ src/main/java/com/example/safariwebstore008/models/CustomerOrder.java
@@ -0,0 +1,36 @@
+git package com.example.safariwebstore008.models;
+
+import com.example.safariwebstore008.common.BaseClass;
+import com.example.safariwebstore008.enums.DeliveryStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class CustomerOrder extends BaseClass {
+
+    private Date deliveryDate;
+
+    private DeliveryStatus deliveryStatus;
+
+    @OneToMany
+    private List<OrderDetails> eachOrderItem;
+
+    private Double sum;
+
+    private Double deliveryFee;
+
+    @OneToOne
+    private DeliveryMethod deliveryMethod;
+
+
+
+
+}
