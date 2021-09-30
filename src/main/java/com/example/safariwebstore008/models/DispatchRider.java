ab43559bdc595e1db7301b36e4afbf0c231a0f96
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-01 00:01:44 +0100

    this add password parameter to dispatchrider class constructor

--- src/main/java/com/example/safariwebstore008/models/DispatchRider.java
+++ src/main/java/com/example/safariwebstore008/models/DispatchRider.java
@@ -14,8 +14,8 @@ import java.util.List;
 @Entity
 public class DispatchRider extends UserModel{
     public DispatchRider( String firstName, String lastName, Date dateOfBirth,
-                          String email, Gender gender,Roles roles) {
-        super (firstName, lastName, dateOfBirth, email, gender,roles);
+                          String email, Gender gender,Roles roles,String password) {
+        super (firstName, lastName, dateOfBirth, email, gender,roles,password);
     }
     public DispatchRider() {
     }
