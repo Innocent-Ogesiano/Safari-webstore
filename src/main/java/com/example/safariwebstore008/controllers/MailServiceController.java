5f349c401ff800dbf04d15f93942606e05396845
Fushioncode <matthew.feranmi@outlook.com>
2021-10-05 13:32:54 +0100

    testing mail service

---
+++ src/main/java/com/example/safariwebstore008/controllers/MailServiceController.java
@@ -0,0 +1,27 @@
+package com.example.safariwebstore008.controllers;
+
+import com.example.safariwebstore008.dto.MailDto;
+import com.example.safariwebstore008.services.MailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+
+@RestController
+@RequestMapping("/send-notification")
+public class MailServiceController {
+    private final MailService mailService;
+
+    public MailServiceController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @PostMapping
+    public void sendNotification(@RequestBody MailDto mailDto) throws MessagingException {
+        this.mailService.sendMail(mailDto);
+        //return null;
+    }
+}
--- src/main/java/com/example/safariwebstore008/dto/MailDto.java
+++ src/main/java/com/example/safariwebstore008/dto/MailDto.java
@@ -1,10 +1,11 @@
 package com.example.safariwebstore008.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class MailDto {
     private String to;
     private String subject;
