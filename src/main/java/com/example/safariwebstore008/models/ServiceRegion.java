6757847f3c2c8e4fa5fd9ccf159815a9115f1395
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-06 20:47:50 +0100

    This add a service region for the dispatch rider to cover

---
+++ src/main/java/com/example/safariwebstore008/models/ServiceRegion.java
@@ -0,0 +1,21 @@
+package com.example.safariwebstore008.models;
+
+import com.example.safariwebstore008.common.BaseClass;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ServiceRegion extends BaseClass {
+   private String RegionName;
+   @OneToMany
+   private List<Cities>CitiesWithinARegion;
+   @OneToOne
+   private  UserModel userModel;
+}
