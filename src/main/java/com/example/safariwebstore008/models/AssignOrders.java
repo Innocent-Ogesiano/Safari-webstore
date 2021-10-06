7b3ef68999851fc59a6cbd7d7d74dd0dcd99de49
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-06 20:47:01 +0100

    this add a class call Assignorders for dispatch rider

---
+++ src/main/java/com/example/safariwebstore008/models/AssignOrders.java
@@ -0,0 +1,18 @@
+package com.example.safariwebstore008.models;
+
+import com.example.safariwebstore008.common.BaseClass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AssignOrders extends BaseClass {
+    @ManyToOne
+    private UserModel user;
+}
