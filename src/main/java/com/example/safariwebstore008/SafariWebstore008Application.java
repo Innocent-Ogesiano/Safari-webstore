98942454ed4c5383f36dda0970b2bbb55633802e
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-06 20:34:04 +0100

    modified

--- src/main/java/com/example/safariwebstore008/SafariWebstore008Application.java
+++ src/main/java/com/example/safariwebstore008/SafariWebstore008Application.java
@@ -1,6 +1,5 @@
 package com.example.safariwebstore008;
 
-import com.example.safariwebstore008.models.Admin;
 import com.example.safariwebstore008.models.UserModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
