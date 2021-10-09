da6f1cdf7e87c2b558914242787fe4cae187a224
Tobi-Java <bolarinwagideon@gmail.com>
2021-10-09 12:47:47 +0100

    login implementation and admin create dispatch rider

--- src/main/java/com/example/safariwebstore008/services/servicesImpl/UserServicesImpl.java
+++ src/main/java/com/example/safariwebstore008/services/servicesImpl/UserServicesImpl.java
@@ -1,4 +1,35 @@
 package com.example.safariwebstore008.services.servicesImpl;
 
-public class UserServicesImpl {
+import com.example.safariwebstore008.dto.RegistrationDto;
+import com.example.safariwebstore008.enums.Roles;
+import com.example.safariwebstore008.models.User;
+import com.example.safariwebstore008.repositories.UserRepository;
+import com.example.safariwebstore008.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserServicesImpl implements UserServices {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User signup(RegistrationDto registrationDto) {
+        User user = new User();
+        user.setFirstName(registrationDto.getFirstName());
+        user.setLastName(registrationDto.getLastName());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+        user.setDateOfBirth(registrationDto.getDateOfBirth());
+        user.setGender(registrationDto.getGender());
+        user.setRoles(Roles.CUSTOMER);
+        user.setIsEnabled(true);
+
+        return userRepository.save(user);
+    }
 }
