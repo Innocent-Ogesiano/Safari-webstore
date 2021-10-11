8ca2fef7fc0b3f8e290db5256fd649edf759e314
Matthew Adesina <57835016+FushionCode@users.noreply.github.com>
2021-10-11 08:31:39 +0100

    Update MailConfig.java

--- src/main/java/com/example/safariwebstore008/configurations/MailConfig.java
+++ src/main/java/com/example/safariwebstore008/configurations/MailConfig.java
@@ -14,10 +14,6 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
-//    @Value("${spring.mail.username}")
-//    private String username;
-//    @Value("{spring.mail.password}")
-//    private String password;
 
     @Bean
     public JavaMailSender mailSender() {
