f24b87f4351b3a103c45ae5ff5eb06723541171f
Innocent-Ogesiano <innocentekweogu@gmail.com>
2021-10-13 09:23:31 +0100

    sends the token through request header for authorization

--- src/main/java/com/example/safariwebstore008/controllers/UserController.java
+++ src/main/java/com/example/safariwebstore008/controllers/UserController.java
@@ -1,29 +1,37 @@
 package com.example.safariwebstore008.controllers;
 
+import com.example.safariwebstore008.configurations.JwtRequestFilter;
 import com.example.safariwebstore008.configurations.JwtTokenUtil;
 import com.example.safariwebstore008.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 public class UserController {
     UriComponentsBuilder uriComponentsBuilder;
     private final UserServices userServices;
     private final JwtTokenUtil jwtTokenUtil;
+    private final JwtRequestFilter requestFilter;
 
     @Autowired
-    public UserController(UserServices userServices, JwtTokenUtil jwtTokenUtil) {
+    public UserController(UserServices userServices, JwtTokenUtil jwtTokenUtil,
+                          JwtRequestFilter requestFilter) {
         this.userServices = userServices;
         this.jwtTokenUtil = jwtTokenUtil;
+        this.requestFilter = requestFilter;
     }
 
-    @GetMapping("/walletBalance/{token}")
-    public ResponseEntity<Double> checkWalletBalance(@PathVariable String token) {
+    @GetMapping("/walletBalance")
+    public ResponseEntity<Double> checkWalletBalance(HttpServletRequest request) throws ServletException, IOException {
+        String token = request.getHeader("Authorization").split(" ")[1];
         String email = jwtTokenUtil.getUserEmailFromToken(token);
         Double walletBalance = userServices.checkWalletBalance(email);
         return new ResponseEntity<>(walletBalance, HttpStatus.OK);
