f5e4862d3c7733a8f1ff0c8162bb256589e9ed2c
Decagon <decagon@Richie.local>
2021-10-13 19:51:57 +0100

    SCA-19 end-point to checkout

--- src/main/java/com/example/safariwebstore008/controllers/UserController.java
+++ src/main/java/com/example/safariwebstore008/controllers/UserController.java
@@ -1,7 +1,47 @@
 package com.example.safariwebstore008.controllers;
 
+import com.example.safariwebstore008.configurations.JwtRequestFilter;
+import com.example.safariwebstore008.configurations.JwtTokenUtil;
+import com.example.safariwebstore008.dto.CheckoutDto;
+import com.example.safariwebstore008.models.CustomerOrder;
+import com.example.safariwebstore008.services.CustomerOrderService;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/customer")
 public class UserController {
-    UriComponentsBuilder uriComponentsBuilder;
+
+    private CustomerOrderService customerOrderService;
+    @Autowired
+    public UserController(CustomerOrderService customerOrderService) {
+        this.customerOrderService = customerOrderService;
+    }
+
+    @PostMapping("/checkout")
+    private ResponseEntity<?> createACustomerOrder(@RequestBody CheckoutDto checkoutDto){
+       CustomerOrder customerOrder=customerOrderService.createACustomerOrder(checkoutDto);
+       if(customerOrder!=null){
+       return new ResponseEntity<>(customerOrder,HttpStatus.CREATED);
+       }else{
+           return new ResponseEntity<>("User not allowed",HttpStatus.FORBIDDEN);
+       }
+    }
 }
---
+++ src/main/java/com/example/safariwebstore008/services/servicesImpl/CustomerOrderServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.safariwebstore008.services.servicesImpl;
+
+import com.example.safariwebstore008.dto.CheckoutDto;
+import com.example.safariwebstore008.enums.DeliveryMethod;
+import com.example.safariwebstore008.enums.DeliveryStatus;
+import com.example.safariwebstore008.enums.OrderAssigStatus;
+import com.example.safariwebstore008.models.CustomerOrder;
+import com.example.safariwebstore008.models.ShipingAddress;
+import com.example.safariwebstore008.models.User;
+import com.example.safariwebstore008.repositories.CustomerOrderRepository;
+import com.example.safariwebstore008.repositories.ShippingRepository;
+import com.example.safariwebstore008.services.CustomerOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+@Service
+public class CustomerOrderServiceImpl implements CustomerOrderService {
+    @Autowired
+    private CustomerOrderRepository customerOrderRepository;
+    @Autowired
+    private ShippingRepository shippingRepository;
+
+    @Override
+    public CustomerOrder createACustomerOrder(CheckoutDto checkoutDto) {
+        CustomerOrder customerOrder = new CustomerOrder();
+        ShipingAddress shipingAddress= new ShipingAddress();
+        User user = checkoutDto.getCart().getUserModel();
+        customerOrder.setDeliveryFee(checkoutDto.getDeliveryFee());
+        customerOrder.setCart(checkoutDto.getCart());
+        customerOrder.setStatus(OrderAssigStatus.UNASSIGNED);
+        customerOrder.setDeliveryStatus(DeliveryStatus.PENDING);
+        customerOrder.setCreateDate(LocalDateTime.now());
+        customerOrder.setTotalOrderAmount(checkoutDto.getTotalOrderAmount());
+        customerOrder.setDeliveryMethod(DeliveryMethod.DOOR_DELIVERY);
+        shipingAddress.setEmail(checkoutDto.getEmail());
+        shipingAddress.setAddress(checkoutDto.getAddress());
+        shipingAddress.setCityName(checkoutDto.getCity());
+        shipingAddress.setUserModel(user);
+        shipingAddress.setRegionName(checkoutDto.getProvince());
+        shipingAddress.setUpdateDate(LocalDateTime.now());
+        shipingAddress.setFirstName(checkoutDto.getFirstName());
+        shipingAddress.setLastName(checkoutDto.getLastName());
+        shipingAddress.setPhoneNumber(checkoutDto.getPhoneNumber());
+        shippingRepository.save(shipingAddress);
+        customerOrder.setShippingAddress(shipingAddress);
+        return customerOrderRepository.save(customerOrder);
+    }
+}
