e5300684a4a46559dd8400a21910bf21069bddf0
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-14 09:01:51 +0100

    delete space

--- src/main/java/com/example/safariwebstore008/services/ProductService.java
+++ src/main/java/com/example/safariwebstore008/services/ProductService.java
@@ -4,6 +4,7 @@ import com.example.safariwebstore008.models.Products;
 
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductService {
     List<Products> adminViewAllProductsPaginated(int page, int size);
