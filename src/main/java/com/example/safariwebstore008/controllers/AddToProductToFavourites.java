0a264a05c8900c66c44603de4a475c4838a5f7df
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-14 08:55:02 +0100

    this remove space and used import

--- src/main/java/com/example/safariwebstore008/controllers/AddToProductToFavourites.java
+++ src/main/java/com/example/safariwebstore008/controllers/AddToProductToFavourites.java
@@ -1,18 +1,12 @@
 package com.example.safariwebstore008.controllers;
-
 import com.example.safariwebstore008.dto.ProductFavouritesDto;
-import com.example.safariwebstore008.models.Products;
 import com.example.safariwebstore008.repositories.ProductRepository;
 import com.example.safariwebstore008.services.servicesImpl.FavouritesServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
