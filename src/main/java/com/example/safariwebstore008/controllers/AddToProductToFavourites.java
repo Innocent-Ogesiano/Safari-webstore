f15ce06341caa5696f0ebc72d6d4facc4840e97f
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-14 08:54:15 +0100

    This  add following package ProductFavouritesDto,.Products, and ProductRepository

--- src/main/java/com/example/safariwebstore008/controllers/AddToProductToFavourites.java
+++ src/main/java/com/example/safariwebstore008/controllers/AddToProductToFavourites.java
@@ -1,2 +1,36 @@
-package com.example.safariwebstore008.controllers;public class AddToProductToFavourites {
+package com.example.safariwebstore008.controllers;
+
+import com.example.safariwebstore008.dto.ProductFavouritesDto;
+import com.example.safariwebstore008.models.Products;
+import com.example.safariwebstore008.repositories.ProductRepository;
+import com.example.safariwebstore008.services.servicesImpl.FavouritesServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@RestController
+public class AddToProductToFavourites {
+    @Autowired
+    ProductRepository productRepository;
+    @Autowired
+    FavouritesServiceImpl favouritesService;
+@PostMapping("/add_favorite")
+    public ResponseEntity<Object> addToFavourites(@RequestBody ProductFavouritesDto productFavouritesDto, HttpServletRequest request){
+    Principal principal = request.getUserPrincipal();
+   String color = productFavouritesDto.getProductColor();
+    String userEmail = principal.getName();
+    System.out.println(userEmail);
+  String message =  favouritesService.findFavouritesByProductsAndUserModel(userEmail,productFavouritesDto);
+  return new ResponseEntity<Object>(message,HttpStatus.OK);
+}
 }
