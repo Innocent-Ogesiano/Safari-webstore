ac03be2ac53d9f57f0f5193633cd9912187d77ef
iyage <iyaghe.ofunrein@decagon.dev>
2021-10-14 09:10:26 +0100

    This add productfavouritesDto to dto folder

---
+++ src/main/java/com/example/safariwebstore008/dto/ProductFavouritesDto.java
@@ -0,0 +1,14 @@
+package com.example.safariwebstore008.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductFavouritesDto {
+    private  String productName;
+    private  String productColor;
+
+}
